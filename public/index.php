6cba165f0be75d0eacb306bb895974b3a70f9c07
Gun Tr <guntr@yahoo.com>
2013-02-09 15:16:26 -0500

    test8

--- public/index.php
+++ public/index.php
@@ -1,19 +1,19 @@
 <?php
 
 function new_freephoneline_crypto_context($Key) {
-    $Key = substr(md5($Key), 0, 16); //The first 16 characters of the hex represntation of the MD5 of the key
-    $IV = 'fedcba9876543210'; //The IV they use
-    if ($CryptoModule = mcrypt_module_open('rijndael-128', '', 'cbc', '')) { //Open the cipher (AES _is_ Rijindael)
-        mcrypt_generic_init($CryptoModule, $Key, $IV); //Initialize
-        return $CryptoModule; //Return
-    } else {
-        die('Initializing mcrypt failed!');
-    }
+	$Key = substr(md5($Key), 0, 16); //The first 16 characters of the hex represntation of the MD5 of the key
+	$IV = 'fedcba9876543210'; //The IV they use
+    if ($CryptoModule = mcrypt_module_open('rijndael-128', '', 'cbc', '')) { //Open the cipher (AES _is_ Rijindael)
+        mcrypt_generic_init($CryptoModule, $Key, $IV); //Initialize
+        return $CryptoModule; //Return
+    } else {
+        die('Initializing mcrypt failed!');
+    }
 }
 
 function delete_freephoneline_crypto_context($CryptoModule) {
-    mcrypt_generic_deinit($CryptoModule);
-    mcrypt_module_close($CryptoModule);
+    mcrypt_generic_deinit($CryptoModule);
+    mcrypt_module_close($CryptoModule);
 }
 
 $SIPUsername = '16042834338';
